7524df51954e57aace7e2e8cf2b176414099893b
zeyad-hamouda <22000660@ug.buid.ac.ae>
2023-04-24 09:39:12 +0400

    commit

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-/*const searchInput = document.querySelector('.search-input');
+const searchInput = document.querySelector('.search-input');
 const searchBtn = document.querySelector('.search-btn');
 
 searchBtn.addEventListener('click', () => {
